bd9d5614cca92118efbb10fdcc6417025cb28bc3
SaturnAnton <157697504+SaturnAnton@users.noreply.github.com>
2026-03-16 09:29:10 +0100

    Algoritmo Q-Learning e visualizzazione del risultato
    Ho inserito all'interno della repository :
    - il file dell'algoritmo (q-learning.py) all'interno del quale eseguo il training dell'agente e salvo la q-table in un determinato file
    - il file di test/esecuzione (visualization.py) che carica la q-table salvata per testare l'agente nell'ambiente MiniGrid in modalità "human", visualizzando le performance apprese.

---
+++ visualization.py
@@ -0,0 +1,39 @@
+import pickle
+import gymnasium as gym
+from minigrid.wrappers import *
+import numpy as np
+import time 
+
+def load(filename):
+    with open(filename,"rb") as handle:
+        qtable = pickle.load(handle)
+    return qtable
+
+def get_state_key(obs):
+    img_flat = obs['image'][:, :, 0].flatten()  # Prende il primo strato della matrice e restituisce una lista monodimensionale
+    direction = obs['direction']
+    return str(list(img_flat) + [direction])
+
+env = gym.make("MiniGrid-DoorKey-5x5-v0", render_mode="human")
+
+obs, info = env.reset()
+s = get_state_key(obs)
+done = False
+
+q_table = load("test.pkl")
+
+while not done:
+    # Durante il test usiamo solo l'azione migliore (argmax), niente epsilon
+    a = np.argmax(q_table[s])
+    
+    obs, r, terminated, truncated, info = env.step(a)
+    s = get_state_key(obs)
+    
+    done = terminated or truncated
+    
+    if r > 0:
+        print("Vittoria!")
+    
+    time.sleep(0.1)
+
+env.close()
