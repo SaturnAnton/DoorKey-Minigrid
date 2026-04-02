c7d680d3ce252544f6b0ee6d0765912e35e68d74
SaturnAnton <157697504+SaturnAnton@users.noreply.github.com>
2026-04-02 16:24:32 +0200

    Modifica codice Groq
    La libreria è stata aggiornata, quindi posso utilizzare il vecchio codice senza specificare il modello direttamente nel codice.

--- VLM/groq.py
+++ VLM/groq.py
@@ -1,11 +1,11 @@
 from PIL import Image
-from monorepo import GroqLLM, load_api_keys
+from monorepo import GroqLLM, load_api_keys, GROQ_MULTIMODAL_MODEL_ID
 
 load_api_keys()
 
-client = GroqLLM(model_id="meta-llama/llama-4-scout-17b-16e-instruct")
+client = GroqLLM(model_id=GROQ_MULTIMODAL_MODEL_ID)
 
-image = Image.open("../figure/env.png")
+image = Image.open("../figure/Verona_Logo.png")
 
 risposta = client.ask(prompt="Cosa vedi in questa immagine?", images=[image])
 print(risposta)
