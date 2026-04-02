8fb403d99f6abe3c68283ad62b11cbe47d346f7e
SaturnAnton <157697504+SaturnAnton@users.noreply.github.com>
2026-04-02 12:38:51 +0200

    Sistemata la cartella della VLM
    Ho inserito all'interno della cartella VLM tutti i vari file

--- VLM/groq.py
+++ VLM/groq.py
@@ -5,7 +5,7 @@ load_api_keys()
 
 client = GroqLLM(model_id="meta-llama/llama-4-scout-17b-16e-instruct")
 
-image = Image.open("figure/env.png")
+image = Image.open("../figure/env.png")
 
 risposta = client.ask(prompt="Cosa vedi in questa immagine?", images=[image])
 print(risposta)
